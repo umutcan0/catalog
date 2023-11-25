192a794dcc8b0468aaa3f0826bc002ecde2d1ae1
umut.bal <umut.bal@etiya.com>
2023-11-25 17:22:45 +0300

    BadRequestAdvice.java updated

---
+++ src/main/java/com/example/catalog/exception/BadRequestAdvice.java
@@ -0,0 +1,28 @@
+package com.example.catalog.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class BadRequestAdvice extends ResponseEntityExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = {BadRequestException.class,RuntimeException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    ResponseEntity<Object> badRequestAdvice(BadRequestException exception) {
+        ErrorMessage errorMessage = new ErrorMessage(
+                404,
+                new Date(),
+                exception.getMessage(),
+                ""
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    }
+}
